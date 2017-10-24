cef860b8fd5f2e0527430f9822fa9ee94d11cfda
Dennis Sørensen <dennissorensen24@hotmail.dk>
2017-10-24 12:24:09 +0200

    Rest client og request

--- WebApiCommunication/Program.cs
+++ WebApiCommunication/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,9 @@ namespace WebApiCommunication
         static void Main(string[] args)
         {
             //Gå ind og hent RestSharp til applicationen, og slet den i packages i solution mappen for din application.
+
+            RestClient client = new RestClient("https://dawa.aws.dk/adresser"); //Lav en client til dit api
+            RestRequest request = new RestRequest("?vejnavn=<indsætVejnavn>&husnr=<indsætHusnr>&struktur=mini", Method.GET); //Lav et request til api'en
 
         }
     }
